74e480bc93387c11477883bc8e59962d1f1b40d1
hooniDad <yoontjr@gmail.com>
2022-06-16 01:42:06 +0900

    Update: app.py - requests, urllib 등에서 발생하는 특정 오류들을 try-except에 반영. 차단을 당하는 건지 뭔지는 모르겠으나, 300초씩 멈추고 재시도 하도록 코딩하였다. - 이 정도가 개선의 한계인 것 같다. 다트의 3단 문서 계층을 regex 없이 html attribute와 js code block 끊어내서 주소 추출하였고, 한 줄씩 읽어 쓰는 방식에서 urllib으로 html 입수한다. 난삽하였던 docKey를 식별값인 서식코드, FSS고유번호, 문서접수번호 체계로 변경하였다. - 회사 정보는 일부 부족한 부분(*)이 있으나 openDart에서 기업개황을 통해 입수한다. * 입수 시점 현행 정보만 얻을수 있다. - 향후 다음 작업한다.  ① Dart_Scraper 쪽에 try-except가 놔둔 것 ② Packaging ③ AWS EC2 생성해서 정기적으로 코드 돌려 DART_Archive 구축 - tqdm은 spyder의 IPython 콘솔 말고 외부 콘솔로 돌려야 설계한대로 나온다.

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ Created on Sun Jun 12 18:38:02 2022
 from Dart_Scraper import Dart_Scraper
 from os import chdir
 import time
+from urllib.error import URLError
+from requests import exceptions
+import ssl
 
 
 def app(i, query):
@@ -23,8 +26,15 @@ def app(i, query):
             i += 1
             if result < 15:  # 해당 페이지 조회수가 15 미만인 경우 마지막 Page
                 break
-    except:
-        print("Error occurred. Wait for 300 seconds.")
+    except (FileNotFoundError,
+            URLError,
+            ConnectionResetError,
+            ConnectionAbortedError,
+            ssl.SSLEOFError,
+            exceptions.SSLError) as e: 
+            # 이 중에 내 문제가 있나? 해결가능한게 있나?
+        
+        print("Wait for 300 seconds. Error occurred: ", e)
         
         time.sleep(300)
         app(i, query)  # 재귀 호출. 에러 발생후 발생 페이지에서 다시 시작 
@@ -41,7 +51,7 @@ query = {
     }
 
 # Page 1에서 Loop 시작
-i = 1
+i = 78
 
 # 시작
 app(i, query)
